ae9ef3263850cf9f8216e61a0bad9e88aeb96b93
Peters <erik.peters@btc-ag.com>
2024-09-17 13:57:11 +0200

    first commit

--- src/main/java/com/btcag/bootcamp/Main.java
+++ src/main/java/com/btcag/bootcamp/Main.java
@@ -1,7 +1,8 @@
 package com.btcag.bootcamp;
 
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! :)");
+        System.out.println("Hello World! :)");
     }
 }
--- src/main/java/com/btcag/bootcamp/xyz.java
+++ src/main/java/com/btcag/bootcamp/xyz.java
@@ -1,2 +1,13 @@
-package com.btcag.bootcamp;public class xyz {
+package com.btcag.bootcamp;
+
+import java.util.Scanner;
+
+public class xyz {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bitte Zahl eingeben zur Quadratzahlberechnung: ");
+        long thenumber = scanner.nextLong();
+        thenumber *= thenumber;
+        System.out.println("Ergebnis: " + thenumber);
+    }
 }
