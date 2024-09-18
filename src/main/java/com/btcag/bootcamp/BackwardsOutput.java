a235ce649d94726cf3768bfa1c5239c2ad0cd96f
Peters <erik.peters@btc-ag.com>
2024-09-18 15:48:46 +0200

    BackwardsOutput fixed

--- src/main/java/com/btcag/bootcamp/BackwardsOutput.java
+++ src/main/java/com/btcag/bootcamp/BackwardsOutput.java
@@ -5,9 +5,25 @@ import java.util.Scanner;
 public class BackwardsOutput {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Bitte geben Sie eine ganze Zahl ein: ");
+        System.out.println("Bitte Zeichenkette eingeben:");
         String input = scanner.nextLine();
         String result = "";
-        System.out.println(input);
+        int length = input.length();
+
+        while (length > 0) {
+            result += input.charAt(length - 1);
+            length--;
+        }
+        System.out.println("Ergebnis String: " + result);
+
+        System.out.println("Bitte Zahl eingeben:");
+        long eingabe = scanner.nextLong();
+
+        long reverse = 0;
+        while( eingabe !=0){
+            reverse = reverse *10 + eingabe%10;
+            eingabe= eingabe/10;
+        }
+        System.out.println("Ergebnis Zahl: " + reverse);
     }
 }
