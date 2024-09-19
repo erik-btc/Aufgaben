a74df99f3550dca0b744d97f28dcb9730dac16b0
Peters <erik.peters@btc-ag.com>
2024-09-19 08:15:21 +0200

    fixed ASCIIKunst1.java

--- src/main/java/com/btcag/bootcamp/ASCIIKunst2.java
+++ src/main/java/com/btcag/bootcamp/ASCIIKunst2.java
@@ -7,14 +7,24 @@ public class ASCIIKunst2 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Bitte geben Sie eine positive Ganzzahl ein: ");
         int zahl = scanner.nextInt();
-        int i = 0;
+        int i = zahl;
+        while (i > 0) {
+            int j = i;
+            while (j > 0) {
+                System.out.print("*");
+                j--;
+            }
+            System.out.println("");
+            i--;
+        }
+        System.out.println();
         while (i < zahl) {
             int j = 0;
             while (j <= i) {
                 System.out.print("*");
                 j++;
             }
-            System.out.println("");
+            System.out.println();
             i++;
         }
     }
