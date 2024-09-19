9cc09ce67dbf275815fd3c6c186312e8e61d7ca3
Peters <erik.peters@btc-ag.com>
2024-09-19 11:08:16 +0200

    file added

--- src/main/java/com/btcag/bootcamp/PrimZahl.java
+++ src/main/java/com/btcag/bootcamp/PrimZahl.java
@@ -2,12 +2,10 @@ package com.btcag.bootcamp;
 
 import java.util.Scanner;
 
-public class Quersumme {
+public class PrimZahl {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Bitte geben Sie eine positive Ganzzahl ein: ");
-        long ganzzahl = scanner.nextLong();
-
+        long zahl = scanner.nextLong();
 
     }
 }
