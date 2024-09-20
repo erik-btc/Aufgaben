55c79259fe7059f239b12767f348835e1112bba9
Peters <erik.peters@btc-ag.com>
2024-09-20 11:28:30 +0200

    new file

---
+++ src/main/java/com/btcag/bootcamp/Primfaktorzerlegung.java
@@ -0,0 +1,10 @@
+package com.btcag.bootcamp;
+
+import java.util.Scanner;
+
+public class Primfaktorzerlegung {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
