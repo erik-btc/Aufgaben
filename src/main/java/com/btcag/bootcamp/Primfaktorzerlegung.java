3c3770578b19dcc21be687bdd6b42c7cf74ebcaa
Peters <erik.peters@btc-ag.com>
2024-09-20 12:58:04 +0200

    fixed Primfaktorzerlegung

--- src/main/java/com/btcag/bootcamp/Primfaktorzerlegung.java
+++ src/main/java/com/btcag/bootcamp/Primfaktorzerlegung.java
@@ -5,6 +5,15 @@ import java.util.Scanner;
 public class Primfaktorzerlegung {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        System.out.println("Bitte geben Sie eine Zahl ein (long): ");
+        long zahl = scanner.nextLong();
+        long teiler = 2;
+        while (zahl > 1) {
+            while (zahl % teiler == 0) {
+                System.out.print(teiler + " ");
+                zahl = zahl / teiler;
+            }
+            teiler++;
+        }
     }
 }
