9f6890dc8dbc7381a8507af5c295f76a773033bc
Peters <erik.peters@btc-ag.com>
2024-09-20 13:49:16 +0200

    added pi task

---
+++ src/main/java/com/btcag/bootcamp/PiAufDefinierteGenauigkeit.java
@@ -0,0 +1,23 @@
+package com.btcag.bootcamp;
+
+import java.util.Scanner;
+
+public class PiAufDefinierteGenauigkeit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bitte geben Sie die Anzahl der Iterationen ein: ");
+        int iterationen = scanner.nextInt();
+        double pi = 0.0;
+        double zaehler = 4.0;
+        double nenner = 1.0;
+        int i = 0;
+        int vorzeichen = 1;
+        while (i < iterationen) {
+            pi = pi + vorzeichen * (zaehler / nenner);
+            nenner = nenner + 2;
+            vorzeichen = vorzeichen * -1;
+            i = i + 1;
+        }
+        System.out.println("Die angenäherte Zahl Pi nach " + iterationen + " Iterationen ist: " + pi);
+    }
+}
