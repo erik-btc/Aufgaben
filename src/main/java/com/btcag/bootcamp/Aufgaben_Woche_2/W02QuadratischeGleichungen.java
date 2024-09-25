faad5b50faf9f9f83a6f0230aa831d53fe14309d
Peters <erik.peters@btc-ag.com>
2024-09-25 14:02:11 +0200

    fixed QuadratischeGleichungen.java

--- src/main/java/com/btcag/bootcamp/Aufgaben_Woche_2/W02QuadratischeGleichungen.java
+++ src/main/java/com/btcag/bootcamp/Aufgaben_Woche_2/W02QuadratischeGleichungen.java
@@ -1,7 +1,26 @@
 package com.btcag.bootcamp.Aufgaben_Woche_2;
 
+import java.util.Scanner;
+
 public class W02QuadratischeGleichungen {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bitte geben Sie a an: ");
+        double a = scanner.nextInt();
+        System.out.println("Bitte geben Sie b an: ");
+        double b = scanner.nextInt();
+        System.out.println("Bitte geben Sie c an: ");
+        double c = scanner.nextInt();
+        double d = (b*b) - (4*a*c);
+        if (d < 0) {
+            System.out.println("Keine Lösung");
+        }
+        else if (d == 0) {
+            System.out.println("Eine Lösung");
+        }
+        else {
+            System.out.println("Zwei Lösungen");
+        }
+        System.out.println(d);
     }
 }
