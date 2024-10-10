8a86a9130e014043bd61f4eb5cfbd38932db46e7
Peters <erik.peters@btc-ag.com>
2024-10-10 13:52:41 +0200

    finished task QueueList

--- src/main/java/com/btcag/bootcamp/Aufgaben_Woche_3/QueueList/Main.java
+++ src/main/java/com/btcag/bootcamp/Aufgaben_Woche_3/QueueList/Main.java
@@ -6,42 +6,56 @@ public class Main {
     public static void main(String[] args) {
         QueueList list = new QueueList();
         Scanner scanner = new Scanner(System.in);
+        int input = 0;
 
-        System.out.println("""
-                Was möchtest du machen ?
+        while(input != 6){
+            System.out.println("""
+                \nWas möchtest du machen ?
                 (1) Das erste Element zurückgeben und löscht es danach.
                 (2) Das letzte Element zurückgeben und löscht es danach.
                 (3) Zahl eingeben und am Ende hinzufügen.
                 (4) Zahl eingeben und am Anfang hinzufügen.
                 (5) Zahl eingeben und das Objekt zurück bekommen, aber nicht löschen.
+                (6) Programm beenden.
                 """);
 
-        int input = scanner.nextInt();
-        int userInput;
+            input = scanner.nextInt();
+            int userInput;
 
-        if (input == 1) {
-            System.out.println("Das Element {queuelist.popFirst()} wurde gelöscht");
+            if (input == 1) {
+                System.out.println("Das Element " + list.popFront() + " wurde gelöscht");
+                list.getList();
+            }
+            else if (input == 2) {
+                System.out.println("Das Element " + list.popLast() + " wurde gelöscht");
+                list.getList();
+            }
+            else if (input == 3) {
+                System.out.println("Was möchtest du hinzufügen?");
+                userInput = scanner.nextInt();
+                System.out.println("Das Objekt " + list.pushLast(userInput) + " wurde der Warteschlange am Ende hinzugefügt!");
+                list.getList();
+            }
+            else if (input == 4) {
+                System.out.println("Was möchtest du hinzufügen?");
+                userInput = scanner.nextInt();
+                System.out.println("Das Objekt " + list.pushFront(userInput) + " wurde der Warteschlange am Anfang hinzugefügt!");
+                list.getList();
+            }
+            else if (input == 5) {
+
+                System.out.println("Welches Element willst du auslesen?");
+                userInput = scanner.nextInt();
+                System.out.println("Das " + userInput + " Element der Queue ist " + list.get(userInput));
+                list.getList();
+            }
+            else if (input == 6) {
+                System.out.println("Programm wird beendet.");
+            }
+            else {
+                System.out.println("Ungültige Zahl.");
+            }
         }
-        else if (input == 2) {
-            System.out.println("Das Element {queuelist.popLast()} wurde gelöscht");
-        }
-        else if (input == 3) {
-            System.out.println("Was möchtest du hinzufügen?");
-            userInput = scanner.nextInt();
-            System.out.println("Das Objekt {queuelist.pushLast(userInput)} wurde der Warteschlange am Ende hinzugefügt!");
-        }
-        else if (input == 4) {
-            System.out.println("Was möchtest du hinzufügen?");
-            userInput = scanner.nextInt();
-            System.out.println("Das Objekt {queuelist.pushFirst(userInput)} wurde der Warteschlange am Anfang hinzugefügt!");
-        }
-        else if (input == 5) {
-            System.out.println("Welches Element willst du auslesen?");
-            userInput = scanner.nextInt();
-            System.out.println("Das {userInput}. Element der Queue ist {queuelist.getObject(userInput)}");
-        }
-        else {
-            System.out.println("Ungültige Zahl.");
-        }
+
     }
 }
